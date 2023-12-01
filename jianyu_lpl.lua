c63e36129520cab34f59c5fbc383a4bae4251798
Baisebaoma <baisebaoma@foxmail.com>
2023-12-01 21:16:29 +0800

    Added voice to Uzi. Bug fixes.

--- jianyu_lpl.lua
+++ jianyu_lpl.lua
@@ -14,13 +14,12 @@ local jianzihao = General(extension, "jianzihao", "god", 3, 3, General.Male)
 local hongwen = fk.CreateFilterSkill{
   name = "hongwen",
   card_filter = function(self, to_select, player)
-    return to_select.suit == Card.Spade or to_select.suit == Card.Club and player:hasSkill(self)
+    return (to_select.suit == Card.Spade or to_select.suit == Card.Club) and player:hasSkill(self)
   end,
   view_as = function(self, to_select)
     if to_select.suit == Card.Club then 
       return Fk:cloneCard(to_select.name, Card.Diamond, to_select.number)
-    end
-    if to_select.suit == Card.Spade then 
+    else -- Spade
       return Fk:cloneCard(to_select.name, Card.Heart, to_select.number)
     end
   end,
@@ -41,7 +40,7 @@ local zouwei = fk.CreateDistanceSkill{
     return 0
   end,
 }
--- 下面这玩意还没写好，先别动
+
 local zouwei_audio = fk.CreateTriggerSkill{
   name = "#zouwei_audio",
 
@@ -61,6 +60,47 @@ local zouwei_audio = fk.CreateTriggerSkill{
   end,
 }
 zouwei:addRelatedSkill(zouwei_audio)
+
+
+-- 圣弩
+-- 参考自formation包的君刘备
+local shengnu = fk.CreateTriggerSkill{
+  name = "shengnu",
+  anim_type = 'drawcard',
+  events = {fk.AfterCardsMove},
+  frequency = Skill.Compulsory,
+  can_trigger = function(self, event, target, player, data)
+    if not player:hasSkill(self) then return false end
+    for _, move in ipairs(data) do
+      if move.to ~= player.id and (move.toArea == Card.PlayerEquip or move.toArea == Card.DiscardPile) then
+        for _, info in ipairs(move.moveInfo) do
+          if Fk:getCardById(info.cardId).name == "crossbow" then
+            return true
+          end
+        end
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local ids = {}
+    for _, move in ipairs(data) do
+      if move.to ~= player.id and (move.toArea == Card.PlayerEquip or move.toArea == Card.DiscardPile) then
+        for _, info in ipairs(move.moveInfo) do
+          if Fk:getCardById(info.cardId).name == "crossbow" then
+            table.insert(ids, info.cardId)
+          end
+        end
+      end
+    end
+    local dummy = Fk:cloneCard("dilu")
+    dummy:addSubcards(ids)
+    player.room:obtainCard(player, dummy, true, fk.ReasonPrey)
+  end,
+}
+
+
+-- 转会
+
 
 -- 洗澡
 local xizao = fk.CreateTriggerSkill{
@@ -94,7 +134,7 @@ local xizao = fk.CreateTriggerSkill{
 
 jianzihao:addSkill(hongwen)
 jianzihao:addSkill(zouwei)
--- jianzihao:addSkill(shengnu)
+jianzihao:addSkill(shengnu)
 -- jianzihao:addSkill(zhuanhui)
 jianzihao:addSkill(xizao)
 -- jianzihao:addSkill(kaiju)
@@ -105,16 +145,20 @@ Fk:loadTranslationTable{
 
   ["hongwen"] = "红温",
   [":hongwen"] = "锁定技，你的♠牌视为<font color='red'>♥</font>牌，你的♣牌视为<font color='red'>♦</font>牌。",
-  ["$hongwen1"] = "哼……",
+  ["$hongwen1"] = "唉，不该出水银的。",
+  ["$hongwen2"] = "哎，兄弟我为什么不打四带两对啊，兄弟？",
+  ["$hongwen3"] = "好难受啊！",
+  ["$hongwen4"] = "操，可惜！",
+  ["$hongwen4"] = "那他咋想的呀？",
 
   ["zouwei"] = "走位",
   [":zouwei"] = "锁定技，当你的装备区没有牌时，其他角色计算与你的距离时，始终+1；当你的装备区有牌时，你计算与其他角色的距离时，始终-1。",
-  ["$zouwei1"] = "冲刺，冲！",
-  ["$zouwei2"] = "别杀我，我错了！",
+  ["$zouwei1"] = "玩一下，不然我是不是一张牌没有出啊兄弟？",
+  ["$zouwei2"] = "完了呀！",
 
   ["shengnu"] = "圣弩",
   [":shengnu"] = "锁定技，当【诸葛连弩】移至弃牌堆或其他角色的装备区时，你获得此【诸葛连弩】。",
-  ["$shengnu1"] = "让我们来猎杀那些陷入黑暗中的人吧！",
+  ["$shengnu1"] = "哎兄弟们我这个牌不能拆吧？",
 
   ["zhuanhui"] = "转会",
   [":zhuanhui"] = "当你的体力值减少时，你可以变更势力。你无法变更为已经成为过的势力。",
@@ -122,14 +166,14 @@ Fk:loadTranslationTable{
 
   ["xizao"] = "洗澡",
   [":xizao"] = "限定技，当你处于濒死状态时，你可以将体力恢复至1，摸三张牌，然后翻面。",
-  ["$xizao1"] = "这游戏玩不玩无所谓了，洗澡去了！",
-  ["$xizao2"] = "待我洗澡归来，又是乱杀之时！",
+  ["$xizao1"] = "怎么赢啊？你别瞎说啊，兄弟们。",
+  ["$xizao2"] = "也不是稳赢吧，我觉得赢了！",
 
   ["kaiju"] = "开局",
-  [":kaiju"] = "<strong>主公技</strong>，锁定技，当你的回合开始时，与你势力不同的武将需要交给你一张牌（没有牌则不用交），并视为对你使用一张【杀】。",
-  ["$kaiju1"] = "从未有如此美妙的开局！",
+  [":kaiju"] = "主公技，锁定技，当你的回合开始时，与你势力不同的武将需要交给你一张牌（没有牌则不用交），并视为对你使用一张【杀】。",
+  ["$kaiju1"] = "不是啊，我在一对鬼的时候我在打什么打一对是一对是他4个9炸我我不输了吗？",
 
-  ["~jianzihao"] = "又没能……突破八强……",
+  ["~jianzihao"] = "好像又要倒下了……",
 }
 
 
