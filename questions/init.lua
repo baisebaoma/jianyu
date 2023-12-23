45ed3ccfdaadac4ffc36c2cc49bd3dab2f10d8bc
Baisebaoma <baisebaoma@foxmail.com>
2023-12-23 22:18:11 +0800

    New questions added

--- questions/init.lua
+++ questions/init.lua
@@ -7,5 +7,6 @@ local q6 = require "packages/jianyu/questions/1678839702470"
 local q7 = require "packages/jianyu/questions/1517368077152"
 local q8 = require "packages/jianyu/questions/1517368116649"
 local q9 = require "packages/jianyu/questions/1575887944358vgv"
+local q10 = require "packages/jianyu/questions/1640265343201"
 
-return {q1, q2, q3, q4, q5, q6, q7, q8, q9}
+return {q1, q2, q3, q4, q5, q6, q7, q8, q9, q10}
