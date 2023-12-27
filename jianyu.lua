ae182e9fe5e0ebd26d10215de492dbef2bab3a6a
baisebaoma <baisebaoma@foxmail.com>
2023-12-27 13:50:40 +0800

    Adjust Ayato

--- jianyu.lua
+++ jianyu.lua
@@ -2393,7 +2393,7 @@ local jy_yuanshen = fk.CreateTriggerSkill{
 
 tym__raiden:addSkill(jy_leiyan)
 tym__raiden:addSkill(jy_zhenshuo)
--- tym__raiden:addSkill(jy_yuanshen)  -- 赋给了雷电将军，别的用引号
+tym__raiden:addSkill(jy_yuanshen)  -- 赋给了雷电将军，别的用引号
 
 Fk:loadTranslationTable {
   ["tym__raiden"] = "雷电将军",
@@ -2460,22 +2460,22 @@ local jy_jinghua = fk.CreateTriggerSkill{
     if data.jinghua_use then
       data.jinghua_use.extraUse = true  -- 加上这个，就可以让它不计入次数了
       room:setPlayerMark(player, "@jy_jinghua", "")
-      room:changeMaxHp(player, 2)  -- 先加体力上限
+      room:changeMaxHp(player, 1)  -- 先加体力上限
       room:recover({  -- 再回复体力
         who = player,
-        num = 2,
+        num = 1,
         recoverBy = player,
         skillName = self.name,
       })
-      room:delay(1000)
+      room:delay(400)
       room:useCard(data.jinghua_use)
     end
     -- 其他的交给别的函数
   end,
   on_refresh = function(self, event, target, player, data)
     local room = player.room
-    room:loseHp(player, 2, self.name)  -- 先失去体力
-    room:changeMaxHp(player, -2)  -- 再减体力上限
+    room:loseHp(player, 1, self.name)  -- 先失去体力
+    room:changeMaxHp(player, -1)  -- 再减体力上限
     room:setPlayerMark(player, "@jy_jinghua", 0)
   end,
 }
@@ -2488,8 +2488,8 @@ local jy_jianying = fk.CreateTriggerSkill{
   events = {fk.EventPhaseEnd},
   can_trigger = function(self, event, target, player, data)
     -- 任何一个人回合都要发动
-    return player:hasSkill(self) and target ~= player and
-      target.phase == Player.Finish and  -- 如果是这个人的回合结束阶段
+    return player:hasSkill(self) and
+      target.phase == Player.Finish and  -- 如果是这个人的结束阶段
       #player:getCardIds(Player.Hand) < player.hp
   end,
   on_use = function(self, event, target, player, data)
@@ -2499,6 +2499,7 @@ local jy_jianying = fk.CreateTriggerSkill{
 
 tym__ayato:addSkill(jy_jinghua)
 tym__ayato:addSkill(jy_jianying)
+tym__ayato:addSkill("jy_yuanshen")  -- 赋给了雷电将军，别的用引号
 
 --[[
   获取原神配音方法：去下面网站找到你的角色的页面，然后查看源文件，搜索配音即可。
@@ -2510,14 +2511,14 @@ Fk:loadTranslationTable {
   ["~tym__ayato"] = "世事无常……",
 
   ["jy_jinghua"] = "镜花",
-  [":jy_jinghua"] = [[每回合限一次，使用或打出基本牌后，你可以使用一张不计入使用次数的【杀】。若如此做，你获得2点体力上限和2点体力，持续到当前角色的回合结束。]],
+  [":jy_jinghua"] = [[每回合限一次，使用或打出基本牌后，你可以使用一张不计入使用次数的【杀】。若如此做，你获得1点体力上限和1点体力，持续到当前角色的回合结束。]],
   ["@jy_jinghua"] = "镜花",
   ["$jy_jinghua1"] = "苍流水影。",
   ["$jy_jinghua2"] = "剑影。",
   ["#jy_jinghua_use"] = "镜花：你可以立即使用一张不计入使用次数的【杀】",
 
   ["jy_jianying"] = "渐盈",
-  [":jy_jianying"] = [[锁定技，其他角色的结束阶段，若你的手牌数小于体力值，你摸一张牌。]],
+  [":jy_jianying"] = [[锁定技，所有角色的结束阶段，若你的手牌数小于体力值，你摸一张牌。]],
   ["$jy_jianying1"] = "冒进是大忌。",
   ["$jy_jianying2"] = "呵……余兴节目。",
 }
