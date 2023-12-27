32dffc7f37cd510de33f854d9b0ad49c555b627c
baisebaoma <baisebaoma@foxmail.com>
2023-12-27 12:57:59 +0800

    Nerfed Ayato

--- jianyu.lua
+++ jianyu.lua
@@ -2460,10 +2460,10 @@ local jy_jinghua = fk.CreateTriggerSkill{
     if data.jinghua_use then
       data.jinghua_use.extraUse = true  -- 加上这个，就可以让它不计入次数了
       room:setPlayerMark(player, "@jy_jinghua", "")
-      room:changeMaxHp(player, 1)  -- 先加体力上限
+      room:changeMaxHp(player, 2)  -- 先加体力上限
       room:recover({  -- 再回复体力
         who = player,
-        num = 1,
+        num = 2,
         recoverBy = player,
         skillName = self.name,
       })
@@ -2473,8 +2473,8 @@ local jy_jinghua = fk.CreateTriggerSkill{
   end,
   on_refresh = function(self, event, target, player, data)
     local room = player.room
-    room:loseHp(player, 1, self.name)  -- 先失去体力
-    room:changeMaxHp(player, -1)  -- 再减体力上限
+    room:loseHp(player, 2, self.name)  -- 先失去体力
+    room:changeMaxHp(player, -2)  -- 再减体力上限
     room:setPlayerMark(player, "@jy_jinghua", 0)
   end,
 }
@@ -2489,7 +2489,7 @@ local jy_jianying = fk.CreateTriggerSkill{
     -- 任何一个人回合都要发动
     return player:hasSkill(self)
       and target.phase == Player.Finish and  -- 如果是这个人的回合结束阶段
-      #player:getCardIds(Player.Hand) < player.hp
+      #player:getCardIds(Player.Hand) < player.hp // 2
   end,
   on_use = function(self, event, target, player, data)
     player:drawCards(1)
@@ -2509,14 +2509,14 @@ Fk:loadTranslationTable {
   ["~tym__ayato"] = "世事无常……",
 
   ["jy_jinghua"] = "镜花",
-  [":jy_jinghua"] = [[每回合限一次，使用或打出基本牌后，你可以使用一张不计入使用次数的【杀】。若如此做：你增加一点体力上限然后回复一点体力；当前角色的回合结束时，你失去一点体力然后减少一点体力上限。]],
+  [":jy_jinghua"] = [[每回合限一次，使用或打出基本牌后，你可以使用一张不计入使用次数的【杀】。若如此做：你获得2点体力上限和2点体力，持续到当前角色的回合结束。]],
   ["@jy_jinghua"] = "镜花",
   ["$jy_jinghua1"] = "苍流水影。",
   ["$jy_jinghua2"] = "剑影。",
   ["#jy_jinghua_use"] = "镜花：你可以立即使用一张不计入使用次数的【杀】",
 
   ["jy_jianying"] = "渐盈",
-  [":jy_jianying"] = [[锁定技，每名角色的结束阶段，若你的手牌数小于体力值，你摸一张牌。]],
+  [":jy_jianying"] = [[锁定技，每名角色的结束阶段，若你的手牌数小于体力值的一半（向下取整），你摸一张牌。]],
   ["$jy_jianying1"] = "冒进是大忌。",
   ["$jy_jianying2"] = "呵……余兴节目。",
 }
