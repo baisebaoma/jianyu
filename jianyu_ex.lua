5845669e53cfbb5040c1f558e67a71d418b5e507
baisebaoma <baisebaoma@foxmail.com>
2024-01-29 23:38:00 +0800

    Enabled Liaoran

--- jianyu_ex.lua
+++ jianyu_ex.lua
@@ -6,8 +6,8 @@ Fk:loadTranslationTable {
   ["jy_ex"] = [[简浴测试]],
 }
 
-local liaoran = General(extension, "jy_ex__liaoran", "god", 3)
-liaoran.hidden = true
+local liaoran = General(extension, "jy_ex__liaoran", "god", 2)
+-- liaoran.hidden = true
 
 local jy_fuzhu = fk.CreateTriggerSkill {
   name = "jy_fuzhu",
@@ -19,11 +19,12 @@ local jy_fuzhu = fk.CreateTriggerSkill {
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
+    -- room:changeMaxHp(player, -1)
     -- 显示对话框，要求回复一个技能名字。最好能做查询，查出来是否真的是需要的技能
     -- TODO：UI变好看一点
     local skill = room:askForCustomDialog(player, self.name,
       "packages/jianyu/qml/fuzhu.qml")
-    -- room:doBroadcastNotify("ShowToast", player.general.name .. "发动“服主”获得了一个新技能" .. skill .. "！")
+    room:doBroadcastNotify("ShowToast", "了然发动“服主”获得了一个新技能" .. skill .. "！")
     room:handleAddLoseSkills(player, skill, nil, true, false)
   end,
 }
@@ -34,7 +35,7 @@ Fk:loadTranslationTable {
   ["jy_ex__liaoran"] = [[了然]],
 
   ["jy_fuzhu"] = "服主",
-  [":jy_fuzhu"] = [[准备阶段，你可以获得服务器上任意一个技能。<br><font size="1">你需要知道这个技能的name参数（如：paoxiao、jy_lingfu、mou__tieji）。若你输入错误，你不会获得任何技能。</font>]],
+  [":jy_fuzhu"] = [[准备阶段，你可以获得服务器上任意一个技能。<br><font size="1">你需要知道这个技能的name参数（如：标张飞的咆哮paoxiao、简浴藿藿的灵符jy_lingfu、谋马超的铁骑mou__tieji）。若输入错误，你不会获得技能。</font>]],
 
   ["jy_diaoxian"] = "掉线",
   [":jy_diaoxian"] = [[锁定技，所有角色的准备阶段，其判定，若点数为：J，跳过判定阶段；Q，跳过摸牌阶段；K，跳过出牌阶段；A，跳过弃牌阶段。其获得该判定牌。]],
