35f37974fff13930195cb0a0877326144a124e9b
baisebaoma <baisebaoma@foxmail.com>
2024-01-29 22:39:28 +0800

    Liaoran function finished

--- jianyu_ex.lua
+++ jianyu_ex.lua
@@ -20,6 +20,7 @@ local jy_fuzhu = fk.CreateTriggerSkill {
   on_use = function(self, event, target, player, data)
     local room = player.room
     -- 显示对话框，要求回复一个技能名字。最好能做查询，查出来是否真的是需要的技能
+    -- TODO：UI变好看一点
     local skill = room:askForCustomDialog(player, self.name,
       "packages/jianyu/qml/fuzhu.qml")
     room:doBroadcastNotify("ShowToast", player.general.name .. "发动“服主”获得了一个新技能" .. skill .. "！")
@@ -30,10 +31,10 @@ local jy_fuzhu = fk.CreateTriggerSkill {
 liaoran:addSkill(jy_fuzhu)
 
 Fk:loadTranslationTable {
-  ["jy_ex__liaoran"] = [[了然测试版]],
+  ["jy_ex__liaoran"] = [[了然]],
 
   ["jy_fuzhu"] = "服主",
-  [":jy_fuzhu"] = [[（这个武将不会出现在选将框）准备阶段，你可以获得一个服务器上的技能。<br>你需要知道这个技能的name参数（如：paoxiao、jy_lingfu）。若你输入错误，你不会获得任何技能。]],
+  [":jy_fuzhu"] = [[准备阶段，你可以获得一个服务器上的技能。<br><font size="1">你需要知道这个技能的name参数（如：paoxiao、jy_lingfu）。若你输入错误，你不会获得任何技能。</font>]],
 
   ["jy_diaoxian"] = "掉线",
   [":jy_diaoxian"] = [[锁定技，所有角色的准备阶段，其判定，若点数为：J，跳过判定阶段；Q，跳过摸牌阶段；K，跳过出牌阶段；A，跳过弃牌阶段。其获得该判定牌。]],
