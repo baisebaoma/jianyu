514f1cb068e1fd3ac6b2524e1f08a110f60301cb
baisebaoma <baisebaoma@foxmail.com>
2024-03-06 00:26:45 +0800

    Bug fixes

--- jianyu_kg.lua
+++ jianyu_kg.lua
@@ -395,11 +395,16 @@ local jy_guina_refresh = fk.CreateTriggerSkill {
         return data.from == player.id
       end
     else
-      return data.from == player.id and room:getPlayerById(data.to):getMark("@jy_guina") ~= 0
+      return data.from == player.id and room:getPlayerById(data.to):getMark("@jy_guina-phase") ~= 0
     end
   end,
   on_refresh = function(self, event, target, player, data)
     local room = player.room
+    room:doAnimate("InvokeSkill", {
+      name = "jy_guina",
+      player = player.id,
+      skill_type = "offensive",
+    })
     if event == fk.TargetSpecified then
       local guina_players = {}
       local targets = {}
