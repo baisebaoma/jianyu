57f2bb0fcfa46651d15615a9852a9a08eeaea3fe
baisebaoma <baisebaoma@foxmail.com>
2024-03-08 15:22:33 +0800

    Added xuyu

--- jianyu_tg.lua
+++ jianyu_tg.lua
@@ -5,6 +5,188 @@ Fk:loadTranslationTable {
   ["jianyu_tg"] = [[简浴-集思广益]],
 }
 
+local zaisheng = fk.CreateTriggerSkill {
+  name = "jy_zaisheng",
+  anim_type = "support",
+  events = { fk.AfterCardsMove, fk.Damaged },
+  can_trigger = function(self, event, target, player, data)
+    if not player:hasSkill(self) then return false end
+    if event == fk.AfterCardsMove then
+      for _, move in ipairs(data) do
+        if move.moveReason ~= fk.ReasonUse and move.from then -- and move.moveVisible 可能需要加上技能描述里没有的moveVisible，因为如果是背面朝上的，你不知道这是红色，就不应该发动这个技能
+          for _, info in ipairs(move.moveInfo) do
+            if (info.fromArea == Card.PlayerHand or info.fromArea == Card.PlayerEquip) and
+                Fk:getCardById(info.cardId).color == Card.Red then
+              data.jy_zaisheng_moveFrom = move.from
+              return true
+            end
+          end
+        end
+      end
+    else -- fk.Damaged
+      return target:getMark("@jy_zaisheng") ~= 0 and data.to:getMark("jy_zaisheng_triggered-round") == 0
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    if event == fk.AfterCardsMove then
+      return player.room:askForSkillInvoke(player, self.name, nil, "#jy_zaisheng_prompt::" .. data.jy_zaisheng_moveFrom)
+    else -- fk.Damaged
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    if event == fk.AfterCardsMove then
+      room:doIndicate(player.id, { data.jy_zaisheng_moveFrom }) -- 播放指示线，代表我给你上了buff
+      local jy_zaisheng_moveFrom = room:getPlayerById(data.jy_zaisheng_moveFrom)
+      room:recover({
+        who = jy_zaisheng_moveFrom,
+        num = 1,
+        recoverBy = player,
+        skillName = self.name,
+      })
+      room:setPlayerMark(jy_zaisheng_moveFrom, "@jy_zaisheng", "")
+    else -- fk.Damaged
+      if data.card then
+        local room = player.room
+        local subcards = data.card:isVirtual() and data.card.subcards or { data.card.id }
+        if #subcards > 0 and table.every(subcards, function(id) return room:getCardArea(id) == Card.Processing end) then
+          room:obtainCard(player.id, data.card, true, fk.ReasonJustMove)
+        end
+      end
+      if data.from then
+        local cards = {}
+        for _, i in ipairs(data.from:getCardIds(Player.Hand)) do
+          if Fk:getCardById(i).is_damage_card then
+            table.insert(cards, i)
+          end
+        end
+        if #cards > 0 then
+          room:obtainCard(player.id, cards[math.random(#cards)], true, fk.ReasonJustMove)
+        end
+      end
+      room:setPlayerMark(data.to, "jy_zaisheng_triggered-round", 1)
+    end
+  end,
+  refresh_events = { fk.EventPhaseChanging },
+  can_refresh = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self) and
+        data.to == Player.Start
+  end,
+  on_refresh = function(self, event, target, player, data)
+    local room = player.room
+    for _, p in ipairs(room:getAlivePlayers()) do
+      if p:getMark("@jy_zaisheng") ~= 0 then
+        room:setPlayerMark(p, "@jy_zaisheng", 0)
+      end
+    end
+  end,
+}
+
+local zhushe = fk.CreateTriggerSkill {
+  name = "jy_zhushe",
+  anim_type = 'drawcard',
+  events = { fk.EventPhaseStart, fk.CardUsing, fk.Damage },
+  mute = true,
+  can_trigger = function(self, event, target, player, data)
+    if player:hasSkill(self) and target == player then
+      if event == fk.EventPhaseStart then
+        return player.phase == Player.Start
+      elseif event == fk.CardUsing then
+        return player:getMark("@jy_zhushe-turn") ~= 0
+      else -- fk.Damage
+        return player:getMark("@jy_zhushe-turn") ~= 0 and not data.to.dead
+      end
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    if event == fk.EventPhaseStart then
+      return player.room:askForSkillInvoke(player, self.name)
+    else -- two events
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    if event == fk.EventPhaseStart then
+      player:broadcastSkillInvoke(self.name)
+      room:doAnimate("InvokeSkill", {
+        name = self.name,
+        player = player.id,
+        skill_type = "drawcard",
+      })
+      local cards = room:askForCard(player, 1, 999, true, self.name, true, nil, "#jy_zhushe_prompt", nil, true)
+      if #cards > 0 then
+        room:throwCard(cards, self.name, player, player)
+        if player:isAlive() then
+          player:drawCards(#cards, self.name)
+          room:setPlayerMark(player, "@jy_zhushe-turn", "")
+        end
+      end
+    elseif event == fk.CardUsing then
+      -- player:broadcastSkillInvoke(self.name)  -- 这个就别播语音了，不然无法响应+造成伤害一张牌播两遍语音很吵
+      room:doAnimate("InvokeSkill", {
+        name = self.name,
+        player = player.id,
+        skill_type = "offensive",
+      })
+      data.disresponsiveList = table.map(room.alive_players, Util.IdMapper)
+    else              -- fk.Damaged
+      room:delay(200) -- 不加delay的话，在放AOE卡牌时一瞬间有太多事件，会出现卡顿
+      player:broadcastSkillInvoke(self.name)
+      room:doAnimate("InvokeSkill", {
+        name = self.name,
+        player = player.id,
+        skill_type = "drawcard",
+      })
+      room:recover({
+        who = data.to,
+        num = data.damage,
+        recoverBy = player,
+        skillName = self.name,
+      })
+      data.to:drawCards(2, self.name)
+    end
+  end,
+}
+local zhushe_mod = fk.CreateTargetModSkill {
+  name = "#jy_zhushe_prompt_mod",
+  bypass_times = function(self, player, skill, scope, card, to)
+    return player:hasSkill(self) and player:getMark("@jy_zhushe-turn") ~= 0
+  end,
+  bypass_distances = function(self, player, skill, card, to)
+    return player:hasSkill(self) and player:getMark("@jy_zhushe-turn") ~= 0
+  end,
+}
+zhushe:addRelatedSkill(zhushe_mod)
+
+local xuyu = General(extension, "jy__xuyu", "qun", 3, 3, General.Female)
+xuyu:addSkill(zaisheng)
+xuyu:addSkill(zhushe)
+
+Fk:loadTranslationTable {
+  ["jy__xuyu"] = "絮雨",
+  ["#jy__xuyu"] = "巡游医师",
+  ["designer:jy__xuyu"] = "emo公主",
+  ["cv:jy__xuyu"] = "刘十四",
+  ["illustrator:jy__xuyu"] = "未知",
+  ["~jy__xuyu"] = [[我熟悉这死亡的气息……]],
+
+  ["jy_zaisheng"] = "再生",
+  ["@jy_zaisheng"] = "再生",
+  ["#jy_zaisheng_prompt"] = [[是否发动〖再生〗令 %dest 回复一点体力，然后你获得〖再生〗的增益效果？]],
+  [":jy_zaisheng"] = [[每当一名角色不因使用而失去红色牌时，你可以令其回复一点体力。若如此做，直到你的下回合开始：每回合限一次，当该角色受到伤害后，你获得对其造成伤害的牌，并随机获得伤害来源手牌中一张伤害牌。]],
+  ["$jy_zaisheng1"] = [[不要害怕。]],
+  ["$jy_zaisheng2"] = [[让我来消除痛苦。]],
+
+  ["jy_zhushe"] = "注射",
+  ["@jy_zhushe-turn"] = "注射",
+  ["#jy_zhushe_prompt"] = "你可以重铸任意张牌，然后本回合获得〖注射〗的效果",
+  [":jy_zhushe"] = [[出牌阶段开始时，你可以重铸任意张牌。若如此做，本回合：你使用牌无距离和次数限制、不可被响应；你造成伤害后，你令伤害目标回复等量体力并摸两张牌。]],
+  ["$jy_zhushe1"] = [[准备好注射了。]],
+  ["$jy_zhushe2"] = [[我的治疗是不会痛的。]],
+}
+
 local function isFactor(a, b)
   return a ~= 0 and b ~= 0 and b % a == 0
 end
@@ -119,24 +301,6 @@ peixiu:addSkill(zhitu)
 
 
 Fk:loadTranslationTable {
-  ["jy__xuyu"] = "絮雨",
-  ["#jy__xuyu"] = "巡游医师",
-  ["designer:jy__xuyu"] = "emo公主",
-  ["cv:jy__xuyu"] = "刘十四",
-  ["illustrator:jy__xuyu"] = "未知",
-  ["~jy__xuyu"] = [[我熟悉这死亡的气息……]],
-
-  ["jy_zaisheng"] = "再生",
-  [":jy_zaisheng"] = [[每当一名角色不因使用而失去红色牌时，你可以令其回复1体力。若如此做，直到你下回合开始：每回合限一次，当该角色受到伤害后，你获得对其造成伤害的牌，并随机获得伤害来源手牌中一张伤害牌。]],
-  ["$jy_zaisheng1"] = [[不要害怕。]],
-  ["$jy_zaisheng2"] = [[让我来消除痛苦。]],
-
-  ["jy_zhushe"] = "注射",
-  [":jy_zhushe"] = [[出牌阶段开始时，你可以重铸任意张牌。若如此做，本回合：你使用牌无距离和次数限制、无法响应；你造成伤害后，你令伤害目标回复等量体力并摸两张牌。]],
-  ["$jy_zhushe1"] = [[准备好注射了。]],
-  ["$jy_zhushe2"] = [[我的治疗是不会痛的。]],
-
-
   ["jy__peixiu"] = "简裴秀",
   ["#jy__peixiu"] = "晋图开秘",
   ["designer:jy__peixiu"] = "贾文和",
