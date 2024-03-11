b8075705c45647cc49123f099f43471e6fce1ef0
baisebaoma <baisebaoma@foxmail.com>
2024-03-12 02:15:46 +0800

    bug fixes, now working correctly!

--- jianyu_tg.lua
+++ jianyu_tg.lua
@@ -617,8 +617,10 @@ local mumang = fk.CreateProhibitSkill {
   name = "jy_mumang",
   frequency = Skill.Compulsory,
   is_prohibited = function(self, from, to, card)
-    return from:hasSkill(self) and card.trueName == "slash" and from:distanceTo(to) > 1
-    -- from:getNextAlive(true) ~= to and to:getNextAlive(true) ~= from -- 不能指定上家或下家以外的目标
+    if from:hasSkill(self) and card.trueName == "slash" then
+      return from:distanceTo(to) > 1 or                                     -- 不能指定与你距离大于1的角色为目标
+          (from:getNextAlive(true) ~= to and to:getNextAlive(true) ~= from) -- 不能指定上家或下家以外的角色为目标
+    end
   end,
 }
 
@@ -642,7 +644,7 @@ Fk:loadTranslationTable {
   [":jy_zitai"] = [[转换技，锁定技，当你造成或受到伤害时，阳：你判定，若为红色，防止之；阴：此伤害+1。然后你摸两张牌。]],
 
   ["jy_mumang"] = [[目盲]],
-  [":jy_mumang"] = [[锁定技，你的【杀】不能指定与你距离大于1的角色为目标。]],
+  [":jy_mumang"] = [[锁定技，你只能选择位于你的上家或下家且与你的距离不超过1的角色作为【杀】的目标。]],
 
   ["jy_yujian"] = [[预见]],
   [":jy_yujian"] = [[准备阶段开始时，你可以观看牌堆顶的X张牌，然后将任意数量的牌置于牌堆顶，将其余的牌置于牌堆底。（X为游戏轮数且至多为5）。]],
