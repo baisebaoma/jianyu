7756689e1b08356a6af069080563b7f6be1cc3d7
baisebaoma <baisebaoma@foxmail.com>
2024-03-26 19:07:09 +0800

    rm randomseed

--- question.lua
+++ question.lua
@@ -1,10 +1,6 @@
-local Question = require 'packages/jianyu/_base'
-
--- 把randomseed定在这里，不用放到函数里去，反正有一个种子就够了，不用每次都换新的种子。
-math.randomseed(os.time())
-
+local Question = {}
+-- randomseed 已经在主程序里被设定，我们无需设定
 -- TODO：换一个占用空间更小的方式来存储题目及答案，目前有点太大了
-
 Question.question_set = require "packages/jianyu/questions/init"
 
 -- 随机返回一个问题
