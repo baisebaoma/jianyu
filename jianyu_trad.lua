b74c18f121d03c620f73747659a8ce94d2c643e8
Qiuyu Fu <qfu49@gatech.edu>
2024-06-01 18:00:51 +0800

    adjusted caopi

--- jianyu_trad.lua
+++ jianyu_trad.lua
@@ -751,7 +751,6 @@ local fangzhu = fk.CreateTriggerSkill {
 local xingshang = fk.CreateTriggerSkill {
   name = "jy_trad_xingshang",
   anim_type = "offensive",
-  frequency = Skill.Compulsory,
   events = { fk.TurnedOver },
   can_trigger = function(self, event, target, player, data)
     return target.id < 0 and player:hasSkill(self)
@@ -789,7 +788,7 @@ Fk:loadTranslationTable {
   [":jy_trad_fangzhu"] = [[锁定技，你造成或受到伤害后，所有机器人摸X张牌、翻至背面并减X点体力上限（X为伤害值）。]],
 
   ["jy_trad_xingshang"] = [[行殇]],
-  [":jy_trad_xingshang"] = [[锁定技，一名机器人翻面后，你获得其区域内所有牌。]],
+  [":jy_trad_xingshang"] = [[一名机器人翻面后，你可以获得其区域内所有牌。]],
 }
 
 return extension
